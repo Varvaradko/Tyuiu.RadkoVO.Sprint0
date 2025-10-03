ee84b3705b50a94ffc9a95b6b4d5b62e90ac1581
Варвара Радько <Tyuiu.RadkoVO@outlook.com>
2025-10-03 22:14:41 +0500

    Task1

---
+++ Tyuiu.RadkoVO.Sprint0.Task1.V0/Program.cs
@@ -0,0 +1,14 @@
+﻿namespace Tyuiu.RadkoVO.Sprint0.Task1.V0
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World");
+            string name = "Варя";
+            int age = 18;
+            Console.WriteLine("Здраствуйте меня зовут " + name + " мне " + age + " лет!");
+            Console.ReadKey();
+        }
+    }
+}
