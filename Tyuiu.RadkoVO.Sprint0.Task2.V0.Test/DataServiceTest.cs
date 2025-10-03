792f980d56f8e584a1b4652c33f34f3d366b2322
Варвара Радько <Tyuiu.RadkoVO@outlook.com>
2025-10-04 00:08:13 +0500

    Task2

---
+++ Tyuiu.RadkoVO.Sprint0.Task2.V0.Test/DataServiceTest.cs
@@ -0,0 +1,16 @@
+﻿using Tyuiu.RadkoVO.Sprint0.Task2.V0.Lib;
+
+namespace Tyuiu.RadkoVO.Sprint0.Task2.V0.Test
+{
+    [TestClass]
+    public class DataServiceTest
+    {
+        [TestMethod]
+        public void CheckGetMessageValid()
+        {
+            var name = "Варя";
+            var res = DataService.GetMessage(name);
+            Assert.AreEqual("Привет..., Варя", res);
+        }
+    }
+}
---
+++ Tyuiu.RadkoVO.Sprint0.Task2.V0/Program.cs
@@ -0,0 +1,13 @@
+﻿using Tyuiu.RadkoVO.Sprint0.Task2.V0.Lib;
+
+namespace Tyuiu.RadkoVO.Sprint0.Task1.V0
+{ 
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(DataService.GetMessage("Варя"));
+            Console.ReadKey();
+        }
+    }
+}
