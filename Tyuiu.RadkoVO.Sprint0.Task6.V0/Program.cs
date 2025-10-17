3685aed226208bd31187d0fcab48d38c50f3e69c
Варвара Радько <Tyuiu.RadkoVO@outlook.com>
2025-10-17 19:22:55 +0500

    Task6

---
+++ Tyuiu.RadkoVO.Sprint0.Task6.V0/Program.cs
@@ -0,0 +1,20 @@
+﻿using Tyuiu.RadkoVO.Sprint0.Task6.V0.Lib;
+
+namespace Tyuiu.RadkoVO.sprint0.Task6.V0
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] numsArray = new int[] { 1, 2, 3, 4, 5 };
+
+            Console.WriteLine("Сумма элементов массива = " + DataService.AdditionArray(numsArray));
+
+            Console.WriteLine("Рaзность элементов массива = " + DataService.SubtractionArray(numsArray));
+
+            Console.WriteLine("Произведение элементов массива = " + DataService.MultiplicationArray(numsArray));
+
+            Console.ReadKey();
+        }
+    }
+}
